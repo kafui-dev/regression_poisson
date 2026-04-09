b6f270648112e9647c6d3ffe38b7005687bd5c99
kafui-dev <kafui.the.dev@gmail.com>
2026-04-09 13:10:33 +0100

    Add initial project setup with R profile, project configuration, and dependencies
    - Created .Rprofile to source renv for package management
    - Added .gitignore to exclude R project files and directories
    - Initialized regression_poisson.Rproj with default settings
    - Created libraries.R to load necessary R packages
    - Added renv.lock for dependency management
    - Implemented JavaScript and CSS files for enhanced functionality
    - Added initial Quarto document for Poisson regression analysis

---
+++ libraries.R
@@ -0,0 +1,11 @@
+#install.packages("ggplot2")
+library(ggplot2)
+
+#install.packages("gridExtra")
+library(gridExtra)
+
+#install.packages("caTools") 
+library(caTools) # pour la séparation de dataset en parties train et test
+
+#install.packages("MASS") 
+library(MASS) # pour le modèle binomial négatif
